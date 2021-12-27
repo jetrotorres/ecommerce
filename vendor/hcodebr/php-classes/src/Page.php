9bd0d595504d498b6a64f69b0323dd14f00fc1b1
unknown <jetro@JETRO-FIN.SETORTI.INFO.IN>
2021-12-27 15:25:10 -0300

    criamos o pageAdmin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -10,11 +10,11 @@ class Page {
     private $defaults = [
         "data"=>[]
     ];
-    public function __construct($opts = array()) {
+    public function __construct($opts = array(), $tplDir = "/views/") {
         $this->options = array_merge($this->defaults, $opts);
         // config
         $config = array(
-            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . "/views/",
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . $tplDir,
             "cache_dir"     => $_SERVER["DOCUMENT_ROOT"] . "/views-cache/",
             "debug"         => false // set to false to improve the speed
         );
