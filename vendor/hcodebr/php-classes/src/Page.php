f918364b61b2e2a9f9c1998d776343cbecf81a7d
unknown <jetro@JETRO-FIN.SETORTI.INFO.IN>
2021-12-27 19:00:03 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -8,6 +8,8 @@ class Page {
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
     public function __construct($opts = array(), $tplDir = "/views/") {
@@ -24,7 +26,7 @@ class Page {
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+        If ($this->options["header"] === true) $this->tpl->draw("header");
     }
 
     private function setData($data = array()){
@@ -42,7 +44,7 @@ class Page {
 
     public function __destruct(){
 
-        $this->tpl->draw("footer");
+        If ($this->options["footer"] === true) $this->tpl->draw("footer");
 
     }
 
